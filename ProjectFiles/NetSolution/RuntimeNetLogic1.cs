4904603ca48c7613f1c93a0e0b5fe1bbe8177b94
MassimoV <massimo.variolo@rockwellautomation.com>
2025-02-26 12:30:01 +0100

    Added screens and ui objects

---
+++ ProjectFiles/NetSolution/RuntimeNetLogic1.cs
@@ -0,0 +1,33 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.UI;
+using FTOptix.NativeUI;
+using FTOptix.HMIProject;
+using FTOptix.CoreBase;
+using FTOptix.System;
+using FTOptix.Retentivity;
+using FTOptix.NetLogic;
+using FTOptix.SerialPort;
+using FTOptix.UI;
+using FTOptix.Core;
+using FTOptix.WebUI;
+#endregion
+
+public class RuntimeNetLogic1 : BaseNetLogic
+{
+    public override void Start()
+    {
+        // Insert code to be executed when the user-defined logic is started
+    }
+
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined logic is stopped
+    }
+
+    public void GenerateCSV(){
+        
+    }
+}
